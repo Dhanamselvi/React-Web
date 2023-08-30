730c382081ea6e8ceea7c68c720fac437e2172b1
Dhanamselvi <selviace92@gmail.com>
2023-08-30 15:12:23 +0530

    Created the Complicated Nested  Sibling  HTML structure inside the React App

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 /*
-    How do we create the Sibling Element in React
+    How do we create the complicated Nested Sibling Element in React
 
         <div id="parent">
-            <div id="child">
-                <h1>Sibling 1 </h1>
-                <h2>Sibling 2 </h2>
+            <div id="child1">
+                <h1>Child 1 - Sibling 1 </h1>
+                <h2>Child 1 - Sibling 2 </h2>
+            </div>
+             <div id="child2">
+                <h1>Child 2 - Sibling 1 </h1>
+                <h2>Child 2 - Sibling 2 </h2>
             </div>
         </div>
 */
@@ -14,22 +18,41 @@
 const parent = React.createElement(
     "div",
     {id:"parent"},
-    React.createElement(
-        "div",
-        {id:"child"},
-        [
-            React.createElement(
-                "h1",
-                {},
-                "Sibling 1"
-            ),
-            React.createElement(
-                "h2",
-                {},
-                "Sibling 2"
-            ),
-        ]
-    )
+    [
+        React.createElement(
+            "div",
+            {id:"child1"},
+            [
+                React.createElement(
+                    "h1",
+                    {},
+                    "Child 1 - Sibling 1"
+                ),
+                React.createElement(
+                    "h2",
+                    {},
+                    "Child 1 - Sibling 2"
+                ),
+            ]
+        ),
+        React.createElement(
+            "div",
+            {id:"child2"},
+            [
+                React.createElement(
+                    "h1",
+                    {},
+                    "Child 2 - Sibling 1"
+                ),
+                React.createElement(
+                    "h2",
+                    {},
+                    "Child 2 - Sibling 2"
+                ),
+            ]
+        )
+    ]
+    
 );
 
 console.log(parent) // object
