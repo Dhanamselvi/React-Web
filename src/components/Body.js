074ba07a1e54d6fc45c24fedf8bd11bd927246a3
Dhanamselvi <selviace92@gmail.com>
2023-10-16 14:01:48 +0530

    filtered the restaurant with React State Variable useState() Updated the UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,76 +1,77 @@
 import RestaurantCard from "./RestaurantCard"
 import resList from "../utils/mockData"
-
-let listOfRestaurants = [
-    {
-        "info": {
-            "id": "17819",
-            "name": "KFC",
-            "cloudinaryImageId": "56c9ab92bd79745fd152a30fa2525426",
-            "locality": "Thyagaraya Road",
-            "areaName": "T. Nagar",
-            "costForTwo": "₹400 for two",
-            "cuisines": [
-                "Burgers",
-                "Biryani",
-                "American",
-                "Snacks",
-                "Fast Food"
-            ],
-            "avgRating": 4.2,
-            "parentId": "547",
-            "avgRatingString": "4.2",
-            "totalRatingsString": "10K+",
-            "sla": {
-                "deliveryTime": 23,
-                "lastMileTravel": 2.8,
-                "serviceability": "SERVICEABLE",
-                "slaString": "23 mins",
-                "lastMileTravelString": "2.8 km",
-                "iconType": "ICON_TYPE_EMPTY"
-            },
-        },
-    },
-    {
-        "info": {
-            "id": "17820",
-            "name": "Dominos",
-            "cloudinaryImageId": "56c9ab92bd79745fd152a30fa2525426",
-            "locality": "Thyagaraya Road",
-            "areaName": "T. Nagar",
-            "costForTwo": "₹400 for two",
-            "cuisines": [
-                "Burgers",
-                "Biryani",
-                "American",
-                "Snacks",
-                "Fast Food"
-            ],
-            "avgRating": 3.8,
-            "parentId": "547",
-            "avgRatingString": "4.2",
-            "totalRatingsString": "10K+",
-            "sla": {
-                "deliveryTime": 23,
-                "lastMileTravel": 2.8,
-                "serviceability": "SERVICEABLE",
-                "slaString": "23 mins",
-                "lastMileTravelString": "2.8 km",
-                "iconType": "ICON_TYPE_EMPTY"
-            },
-        },
-    },
-];
+import { useState } from "react";
 
 const Body = () => {
+    const [listOfRestaurants,setListOfRestaurants] = useState([
+        {
+            "info": {
+                "id": "17819",
+                "name": "KFC",
+                "cloudinaryImageId": "56c9ab92bd79745fd152a30fa2525426",
+                "locality": "Thyagaraya Road",
+                "areaName": "T. Nagar",
+                "costForTwo": "₹400 for two",
+                "cuisines": [
+                    "Burgers",
+                    "Biryani",
+                    "American",
+                    "Snacks",
+                    "Fast Food"
+                ],
+                "avgRating": 4.2,
+                "parentId": "547",
+                "avgRatingString": "4.2",
+                "totalRatingsString": "10K+",
+                "sla": {
+                    "deliveryTime": 23,
+                    "lastMileTravel": 2.8,
+                    "serviceability": "SERVICEABLE",
+                    "slaString": "23 mins",
+                    "lastMileTravelString": "2.8 km",
+                    "iconType": "ICON_TYPE_EMPTY"
+                },
+            },
+        },
+        {
+            "info": {
+                "id": "17820",
+                "name": "Dominos",
+                "cloudinaryImageId": "56c9ab92bd79745fd152a30fa2525426",
+                "locality": "Thyagaraya Road",
+                "areaName": "T. Nagar",
+                "costForTwo": "₹400 for two",
+                "cuisines": [
+                    "Burgers",
+                    "Biryani",
+                    "American",
+                    "Snacks",
+                    "Fast Food"
+                ],
+                "avgRating": 3.8,
+                "parentId": "547",
+                "avgRatingString": "4.2",
+                "totalRatingsString": "10K+",
+                "sla": {
+                    "deliveryTime": 23,
+                    "lastMileTravel": 2.8,
+                    "serviceability": "SERVICEABLE",
+                    "slaString": "23 mins",
+                    "lastMileTravelString": "2.8 km",
+                    "iconType": "ICON_TYPE_EMPTY"
+                },
+            },
+        },
+    ])
+    
     return (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" 
-                        onClick={()=>{
-                            listOfRestaurants = listOfRestaurants.filter(res => res.info.avgRating > 4);
-                            console.log(listOfRestaurants)
-                         }}>
+                    onClick = {()=>{
+                        setListOfRestaurants(listOfRestaurants.filter(res=>res.info.avgRating > 4));
+                        console.log(listOfRestaurants)
+                    }}    >
                     Top Rated Restaurant
                 </button>
             </div>
@@ -83,4 +84,4 @@ const Body = () => {
     )
 }
 
-export default Body
+export default Body;
